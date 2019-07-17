540d3a4d15a34c8189456445d25c52bde982826d
austinpearson12 <52326121+austinpearson12@users.noreply.github.com>
2019-07-17 17:03:20 -0400

    stuff
    stuff

--- Austins_Blog/Models/comment.cs
+++ Austins_Blog/Models/comment.cs
@@ -8,15 +8,13 @@ namespace Austins_Blog.Models
     public class Comment
     {
         public int Id { get; set; }
-        public int BlogPostId { get; set;}
-        public string AustinId { get; set; } 
+        public int BlogPostId { get; set; }
+        public string AuthorId { get; set; }
         public string Body { get; set; }
         public DateTimeOffset Created { get; set; }
-        public DateTimeOffset? Updaed { get; set; }
+        public DateTimeOffset? Updated { get; set; }
         public string UpdateReason { get; set; }
-
-        //virtual navigation section
-        public virtual BlogPost BlogPost { get; set; }
         public virtual ApplicationUser Author { get; set; }
+        public virtual BlogPost BlogPost { get; set; }
     }
 }
